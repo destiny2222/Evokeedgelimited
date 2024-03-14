32236dff3867ac1da93d085c66d76fc060e0f4c9
destiny2222 <oghenekumeerieme@gmail.com>
2024-03-14 08:29:34 +0100

    update on breadcrumb

--- resources/views/layout/page-header.blade.php
+++ resources/views/layout/page-header.blade.php
@@ -2,7 +2,7 @@
     <h1 class="page-title fw-bold">@yield('page-title', '')</h1>
     <div>
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('dashboard-page') }}">Home</a></li>
             <li class="breadcrumb-item active" aria-current="page">@yield('page-title', '')</li>
         </ol>
     </div>
